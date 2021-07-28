5e9a30d52a6fad6f7300728c540ab4738bddee31
Deshadeeth Thisarana <77653060+deshadeeth-thisarana@users.noreply.github.com>
2021-07-28 15:59:03 +0530

    Shadow Whisper

---
+++ bot.py
@@ -0,0 +1,86 @@
+﻿from telethon import events, TelegramClient, Button
+import logging
+from telethon.tl.functions.users import GetFullUserRequest as us
+import os
+
+
+logging.basicConfig(level=logging.INFO)
+
+API_ID = "",    
+API_HASH = "",
+TOKEN = os.environ.get["TOKEN", None]
+
+Shadow = TelegramClient(
+        "Whisper",
+        api_id=API_ID,
+        api_hash=API_HASH
+        ).start(bot_token=TOKEN)
+
+db = {}
+    
+@bot.on(events.InlineQuery(pattern="wspr"))
+async def inline(event):
+    me = (await bot.get_me()).username
+    try:
+        inp = event.text.split(None, 1)[1]
+        user, msg = inp.split("|")
+    except IndexError:
+        await event.answer(
+                [], 
+                switch_pm=f"@{me} [Username]|[Message]",
+                switch_pm_param="start"
+                )
+    except ValueError:
+        await event.answer(
+                [],
+                switch_pm=f"Give a message too!",
+                switch_pm_param="start"
+                )
+    try:
+        ui = await bot(us(user))
+    except BaseException:
+        await event.answer(
+                [],
+                switch_pm="Invalid User ID/Username",
+                switch_pm_param="start"
+                )
+        return
+    db.update({"user_id": ui.user.id, "msg": msg, "self": event.sender.id})
+    text = f"""
+A Whisper Has Been Sent by Shadow
+To [{ui.user.first_name}](tg://user?id={ui.user.id})!
+Click The Below Button To See The Message!
+**Note:** __Only {ui.user.first_name} can open this!__
+    """
+    dn = event.builder.article(
+            title="Its a secret message! Sssh",
+            description="It's a secret message! Sssh!",
+            text=text,
+            buttons=[
+                [Button.inline(" Show Message! ", data="wspr")]
+                ]
+            )
+    await event.answer(
+            [dn],
+            switch_pm="It's a secret message! Sssh",
+            switch_pm_param="start"
+            )
+
+
+@bot.on(events.CallbackQuery(data="wspr"))
+async def ws(event):
+    user = int(db["user_id"])
+    lol = [int(db["self"])]
+    lol.append(user)
+    if event.sender.id not in lol:
+        await event.answer("🔐 This message is not for you!", alert=True)
+        return
+    msg = db["msg"]
+    if msg == []:
+        await event.anwswer(
+                "Oops!\nIt's looks like message got deleted from my server!", alert=True)
+        return
+    await event.answer(msg, alert=True)
+
+print("Succesfully Started Bot!")
+bot.run_until_disconnected()
